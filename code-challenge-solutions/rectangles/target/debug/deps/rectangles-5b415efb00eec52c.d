758dae243928b0a45015249d818ef53fd2f24f11
Brian Culver <brculver@microsoft.com>
2022-02-04 00:39:40 -0600

    Add rectagles solution

---
+++ code-challenge-solutions/rectangles/target/debug/deps/rectangles-5b415efb00eec52c.d
@@ -0,0 +1,5 @@
+c:\Dev\rust\rust-2hr-intro\code-challenge-solutions\rectangles\target\debug\deps\rectangles-5b415efb00eec52c.rmeta: src\main.rs
+
+c:\Dev\rust\rust-2hr-intro\code-challenge-solutions\rectangles\target\debug\deps\rectangles-5b415efb00eec52c.d: src\main.rs
+
+src\main.rs:
---
+++ code-challenge-solutions/rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+C:\Dev\rust\rust-2hr-intro\code-challenge-solutions\rectangles\target\debug\rectangles.exe: C:\Dev\rust\rust-2hr-intro\code-challenge-solutions\rectangles\src\main.rs
